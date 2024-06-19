1b47f644a7630804ffe269e0d904b945df88e28d
Riya62024 <139709452+Riya62024@users.noreply.github.com>
2024-06-19 22:20:18 +0530

    Defect No:  619 Exception added

---
+++ src/main/java/com/driver/bookMyShow/Exceptions/RunTimeException.java
@@ -0,0 +1,8 @@
+package com.driver.bookMyShow.Exceptions;
+
+public class RunTimeException extends RuntimeException {
+
+    public RunTimeException(String message) {
+        super(message);
+    }
+}
